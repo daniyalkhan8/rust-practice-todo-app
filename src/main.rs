ac74cdaac9bd49ebbdfa06c1880ff312454f5f63
Daniyal Durrani <daniyalkhand16@gmail.com>
2026-06-18 16:05:00 +0500

    feat: Added code for adding a new todo.
    Created structs Todo and JsonTodo for storing all todo and serializing and deserializing the structs into json using serde_json. Added dummy methods for all the other todo operations.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,62 @@
 use std::env;
+use serde::{Deserialize, Serialize};
+
+const TODOS_PATH: &str = "todos.json";
 
 #[derive(Debug)]
 struct UpdateTodo {
     id: u32,
     title: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    id: u32,
+    title: String,
+    done: bool
+}
+
+#[derive(Debug)]
+struct JsonTodos {
+    todos: Vec<Todo>
+}
+
+impl JsonTodos {
+    fn new() -> JsonTodos {
+        if !std::path::Path::new(TODOS_PATH).exists() {
+            std::fs::write(TODOS_PATH, "[]").expect("Failed to create todos.json");
+        }
+
+        let content = std::fs::read_to_string(TODOS_PATH).expect("Failed to read todos.json");
+        let todos: Vec<Todo> = serde_json::from_str(&content).expect("Failed to parse todos.json");
+
+        JsonTodos { todos }
+    }
+
+    fn add(mut self, title: String) {
+        let id: u32;
+        if self.todos.len() > 0 {
+            id = self.todos[self.todos.len() - 1].id + 1
+        } else {
+            id = 0
+        }
+
+        let done = false;
+        let todo = Todo {id, title, done };
+        self.todos.push(todo);
+
+        let todos_updated = serde_json::to_string(&self.todos).unwrap();
+        std::fs::write(TODOS_PATH, todos_updated).expect("Failed to write to todos.json");
+    }
+
+    fn get(self, id: u32) {}
+
+    fn list(self) {
+        println!("{:#?}", self.todos);
+    }
+    fn update(self, todo: UpdateTodo) {}
+    fn done(self, id: u32) {}
+    fn delete(self, id: u32) {}
 }
 
 #[derive(Debug)]
@@ -60,7 +113,12 @@ fn process_env_args(args: &Vec<String>) -> TodoOperations {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let todo_operation: TodoOperations = process_env_args(&args);
+    let todo_operation = process_env_args(&args);
+    let todos = JsonTodos::new();
 
-    println!("{todo_operation:?}")
+    match todo_operation {
+        TodoOperations::Add(title) => todos.add(title),
+        TodoOperations::List => todos.list(),
+        _ => println!("debug")
+    }
 }
