8e43b49f729056e1acde185f38975a3e2ba2793d
Daniyal Durrani <daniyalkhand16@gmail.com>
2026-06-19 16:31:53 +0500

    refactor: Updated the return types and code formatting
    Changed the return type of function process_env_args to return a Result<TodoOPerations, String> type and also removed the Unknown variant from TodoOperation enum.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use std::env;
 use serde::{Deserialize, Serialize};
+use std::env;
 
 const TODOS_PATH: &str = "todos.json";
 
 #[derive(Debug)]
 struct UpdateTodo {
     id: u32,
-    title: String
+    title: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Todo {
     id: u32,
     title: String,
-    done: bool
+    done: bool,
 }
 
 #[derive(Debug)]
 struct JsonTodos {
-    todos: Vec<Todo>
+    todos: Vec<Todo>,
 }
 
 impl JsonTodos {
@@ -42,7 +42,7 @@ impl JsonTodos {
         }
 
         let done = false;
-        let todo = Todo {id, title, done };
+        let todo = Todo { id, title, done };
         self.todos.push(todo);
 
         let todos_updated = serde_json::to_string(&self.todos).unwrap();
@@ -50,7 +50,12 @@ impl JsonTodos {
     }
 
     fn get(self, id: u32) {
-        let todo: Vec<Todo> = self.todos.iter().filter(|todo| todo.id == id).cloned().collect();
+        let todo: Vec<Todo> = self
+            .todos
+            .iter()
+            .filter(|todo| todo.id == id)
+            .cloned()
+            .collect();
         println!("{:#?}", todo);
     }
 
@@ -59,31 +64,43 @@ impl JsonTodos {
     }
 
     fn update(mut self, update_todo: UpdateTodo) {
-        self.todos = self.todos.into_iter().map(|mut todo| {
-            if todo.id == update_todo.id {
-                todo.title = update_todo.title.clone()
-            }
-            todo
-        }).collect();
+        self.todos = self
+            .todos
+            .into_iter()
+            .map(|mut todo| {
+                if todo.id == update_todo.id {
+                    todo.title = update_todo.title.clone()
+                }
+                todo
+            })
+            .collect();
 
         let todos_updated = serde_json::to_string(&self.todos).unwrap();
         std::fs::write(TODOS_PATH, todos_updated).expect("Failed to write to todos.json");
     }
 
     fn done(mut self, id: u32) {
-        self.todos = self.todos.into_iter().map(|mut todo| {
-           if todo.id == id {
-               todo.done = true;
-           }
-            todo
-        }).collect();
+        self.todos = self
+            .todos
+            .into_iter()
+            .map(|mut todo| {
+                if todo.id == id {
+                    todo.done = true;
+                }
+                todo
+            })
+            .collect();
 
         let todos_updated = serde_json::to_string(&self.todos).unwrap();
         std::fs::write(TODOS_PATH, todos_updated).expect("Failed to write to todos.json");
     }
 
     fn delete(mut self, id: u32) {
-        self.todos = self.todos.into_iter().filter(|todo| todo.id != id).collect();
+        self.todos = self
+            .todos
+            .into_iter()
+            .filter(|todo| todo.id != id)
+            .collect();
         let todos_updated = serde_json::to_string(&self.todos).unwrap();
         std::fs::write(TODOS_PATH, todos_updated).expect("Failed to write to todos.json");
     }
@@ -97,53 +114,48 @@ enum TodoOperations {
     Update(UpdateTodo),
     Done(u32),
     Delete(u32),
-    Unknown
 }
 
-fn process_env_args(args: &Vec<String>) -> TodoOperations {
+fn process_env_args(args: &Vec<String>) -> Result<TodoOperations, String> {
     if args.len() < 2 {
-        return TodoOperations::Unknown;
+        return Err("No operation parameters provided. Please provide either add, get, list, update, done or delete.".to_string());
     }
 
     match args[1].as_str() {
-        "add" => {
-            match args.get(2) {
-                Some(title) => TodoOperations::Add(title.clone()),
-                _ => TodoOperations::Unknown
-            }
+        "add" => match args.get(2) {
+            Some(title) => Ok(TodoOperations::Add(title.clone())),
+            _ => Err("Failed to add the todo.".to_string()),
         },
-        "list" => TodoOperations::List,
-        "get" => {
-            match args.get(2).and_then(|s| s.parse().ok()) {
-                Some(id) => TodoOperations::Get(id),
-                _ => TodoOperations::Unknown
-            }
-        }
-        "update" => {
-            match (args.get(2).and_then(|s| s.parse().ok()), args.get(3)) {
-                (Some(id), Some(title)) => TodoOperations::Update(UpdateTodo { id, title: title.clone() }),
-                _ => TodoOperations::Unknown
-            }
-        }
-        "done" => {
-            match args.get(2).and_then(|s| s.parse().ok()) {
-                Some(id) => TodoOperations::Done(id),
-                _ => TodoOperations::Unknown
-            }
-        }
-        "delete" => {
-            match args.get(2).and_then(|s| s.parse().ok()) {
-                Some(id) => TodoOperations::Delete(id),
-                _ => TodoOperations::Unknown
-            }
-        }
-        _ => TodoOperations::Unknown
+        "list" => Ok(TodoOperations::List),
+        "get" => match args.get(2).and_then(|s| s.parse().ok()) {
+            Some(id) => Ok(TodoOperations::Get(id)),
+            _ => Err("Failed to get the todo.".to_string()),
+        },
+        "update" => match (args.get(2).and_then(|s| s.parse().ok()), args.get(3)) {
+            (Some(id), Some(title)) => Ok(TodoOperations::Update(UpdateTodo {
+                id,
+                title: title.clone(),
+            })),
+            _ => Err("Failed to update todo.".to_string()),
+        },
+        "done" => match args.get(2).and_then(|s| s.parse().ok()) {
+            Some(id) => Ok(TodoOperations::Done(id)),
+            _ => Err("Failed to mark todo as done.".to_string()),
+        },
+        "delete" => match args.get(2).and_then(|s| s.parse().ok()) {
+            Some(id) => Ok(TodoOperations::Delete(id)),
+            _ => Err("Failed to delete todo.".to_string()),
+        },
+        _ => Err("Please provide either add, get, list, update, done or delete.".to_string()),
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let todo_operation = process_env_args(&args);
+    let todo_operation = process_env_args(&args).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        std::process::exit(1);
+    });
     let todos = JsonTodos::new();
 
     match todo_operation {
@@ -153,6 +165,5 @@ fn main() {
         TodoOperations::Update(update_todo) => todos.update(update_todo),
         TodoOperations::Done(id) => todos.done(id),
         TodoOperations::Delete(id) => todos.delete(id),
-        TodoOperations::Unknown => eprintln!("Error occurred while performing the operation.")
     }
 }
